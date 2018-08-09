1d70cc578df53e72313494ae6a2d7284e716f6bf
“Mhaventhan” <mhaventhan@gmail.com>
2018-08-09 15:34:26 +0100

    finished dice with throw button

---
+++ js/index.js
@@ -0,0 +1,50 @@
+function dice() {
+
+  var innerSquare = document.getElementsByClassName('innerSquare');
+  var randomNumber = (Math.floor(Math.random()*6)+1);
+  console.log(randomNumber);
+  for (var i = 0; i < innerSquare.length; i++) {
+    innerSquare[i].className = 'innerSquare';
+  }
+
+
+  switch (randomNumber) {
+    case 1:
+    innerSquare[4].className = 'innerSquare dot';
+    break;
+    case 2:
+    innerSquare[2].className = 'innerSquare dot';
+    innerSquare[6].className = 'innerSquare dot';
+    break;
+    case 3:
+    innerSquare[2].className = 'innerSquare dot';
+    innerSquare[6].className = 'innerSquare dot';
+    innerSquare[4].className = 'innerSquare dot';
+    break;
+    case 4:
+    innerSquare[2].className = 'innerSquare dot';
+    innerSquare[6].className = 'innerSquare dot';
+    innerSquare[0].className = 'innerSquare dot';
+    innerSquare[8].className = 'innerSquare dot';
+    break;
+    case 5:
+    innerSquare[2].className = 'innerSquare dot';
+    innerSquare[6].className = 'innerSquare dot';
+    innerSquare[4].className = 'innerSquare dot';
+    innerSquare[0].className = 'innerSquare dot';
+    innerSquare[8].className = 'innerSquare dot';
+    break;
+    case 6:
+    innerSquare[0].className = 'innerSquare dot';
+    innerSquare[2].className = 'innerSquare dot';
+    innerSquare[3].className = 'innerSquare dot';
+    innerSquare[6].className = 'innerSquare dot';
+    innerSquare[5].className = 'innerSquare dot';
+    innerSquare[8].className = 'innerSquare dot';
+    break;
+
+    default:
+
+  }
+}
+dice()
